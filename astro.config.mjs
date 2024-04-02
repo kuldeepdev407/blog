80a7c44e62ee7bd4621a7fd92a4cea3e077899ba
kuldeepdev407 <kuldeepdev407@gmail.com>
2024-04-02 21:14:01 +0530

    updated site info

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.kuldeepdev407.com',
+	site: 'https://www.kuldeepdev407.com',
 	integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kuldeepdev407 Blog';
-export const SITE_DESCRIPTION = 'Welcome to Blog & Post site of Kuldeep Singh';
+export const SITE_TITLE = 'Kuldeepdev407';
+export const SITE_DESCRIPTION = 'Hi am i am Kuldeep this is my personal website where i do post my blogs and do all kind of stuff.';
